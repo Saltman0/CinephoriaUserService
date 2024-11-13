f5525e4e67a364314a4c444d2539f654c27de7e9
Saltman0 <baudoin.mathieu@gmail.com>
2024-11-13 23:03:37 +0100

    Init migration added

---
+++ migrations/0000_init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "user" (
+	"id" integer GENERATED ALWAYS AS IDENTITY (sequence name "user_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"email" varchar NOT NULL,
+	"password" varchar NOT NULL,
+	"firstName" varchar NOT NULL,
+	"lastName" varchar NOT NULL,
+	"phoneNumber" varchar NOT NULL,
+	"role" varchar DEFAULT 'User' NOT NULL
+);
