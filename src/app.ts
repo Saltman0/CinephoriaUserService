f08f8f87c373d235e3e00cfdb0a7d5b7b3d064dc
Saltman0 <baudoin.mathieu@protonmail.com>
2024-12-11 15:54:28 +0100

    app.ts : port updated

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import pino from "pino";
 import userRoutes from "./routes/user.routes";
 
-export const port: number = 3000;
+export const port: number = parseInt(process.env.PORT as string) || 3000;
 
 export const app: Express = express();
 
