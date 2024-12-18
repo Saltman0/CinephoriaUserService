693b621ea4408ac06931a609cf5f36d4be4fe208
Saltman0 <baudoin.mathieu@protonmail.com>
2024-12-18 19:32:31 +0100

    Update user.controller.ts

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,35 @@
 import { Request, Response } from "express";
 import * as userRepository from "../repository/user.repository";
+import jwt from "jsonwebtoken";
+
+export async function login(req: Request, res: Response) {
+    try {
+        const user = await userRepository.findUserByEmailAndPassword(
+            req.body.email,
+            req.body.password
+        );
+
+        if (user === null) {
+            res.status(401).json({error : `Authentication failed : user ${req.body.email} not found.`});
+        } else {
+            // Define a payload with the information you want to include in the token
+            const payload = {
+                email: user[0].email,
+                iss: "1*!uaNzKqrhZadJEcdpVGBH&E2%@g2usRxQ14h13dexDcjh8gMG3yyQ&aM&NpudW"
+            };
+
+            const token = jwt.sign(payload, "@Kv^n3^^ncBZ78$43GM59cfP77Bee*JQpnfRzgbEB2&w1C&3#54#kP5@0dR0DbuR", {
+                expiresIn: "30d"
+            });
+
+            res.status(200).json(token);
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
 
 export async function getUsers(req: Request, res: Response) {
     try {
