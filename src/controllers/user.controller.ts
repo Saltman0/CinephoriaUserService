e970a21fe9cca47372f068a94cc635ee668e7976
Saltman0 <baudoin.mathieu@protonmail.com>
2024-12-29 11:53:22 +0100

    User controller : payload and responses updated

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -19,6 +19,7 @@ export async function loginUser(req: Request, res: Response) {
             // Define a payload with the information you want to include in the token
             const payload = {
                 id: user[0].id,
+                role: user[0].role,
                 iss: process.env.JWT_KEY
             };
 
@@ -92,7 +93,7 @@ export async function createUser(req: Request, res: Response) {
 
 export async function updateUser(req: Request, res: Response) {
     try {
-        const userToUpdate = await userRepository.updateUser(
+        await userRepository.updateUser(
             parseInt(req.params.id),
             req.body.email,
             req.body.password,
@@ -102,7 +103,7 @@ export async function updateUser(req: Request, res: Response) {
             req.body.role
         );
 
-        res.status(200).json(userToUpdate);
+        res.status(200).json({message: `User updated successfully.`});
     } catch (error) {
         if (error instanceof Error) {
             res.status(500).json({ message: error.message });
@@ -112,11 +113,9 @@ export async function updateUser(req: Request, res: Response) {
 
 export async function deleteUser(req: Request, res: Response) {
     try {
-        const userToDelete = await userRepository.deleteUser(
-            parseInt(req.params.id)
-        );
+        await userRepository.deleteUser(parseInt(req.params.id));
 
-        res.status(200).json(userToDelete);
+        res.status(200).json({message: `User deleted successfully.`});
     } catch (error) {
         if (error instanceof Error) {
             res.status(500).json({ message: error.message });
