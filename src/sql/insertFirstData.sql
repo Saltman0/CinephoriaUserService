7fc1164eb51f7af603d03fedfba4a0ae631eda5e
Saltman0 <baudoin.mathieu@protonmail.com>
2025-09-21 18:21:26 +0200

    ✨ InsetFirstData SQL updated

--- src/sql/insertFirstData.sql
+++ src/sql/insertFirstData.sql
@@ -1,16 +1,23 @@
 -- Start the SQL transaction
 BEGIN;
 
--- Insert user
-INSERT INTO "user" ("email", "password", "firstName", "lastName", "phoneNumber", "role")
-VALUES ('baudoin.mathieu@protonmail.com', '0123456789', 'Mathieu', 'Baudoin', '06 11 22 33 44', 'admin'),
-       ('mathieu.baudoin@gmail.com', '9876543210', 'Mathieu', 'Baudoin', '07 98 56 24 01', 'employee'),
-       ('robert.nikios@gmail.com', 'azerty', 'Robert', 'Nikios', '06 54 89 21 96', 'employee'),
-       ('jessica.belini@protonmail.com', '456789123', 'Jessica', 'Belini', '06 22 78 12 32', 'employee'),
-       ('user1@gmail.com', 'wahouSuperMDP', 'Wahou', 'Lou', '06 33 87 22 33', 'user');
--- Insert user
-
-SAVEPOINT user_savepoint;
+    -- Users
+    INSERT INTO "user" ("email", "password", "firstName", "lastName", "phoneNumber", "role")
+    VALUES ('baudoin.mathieu@protonmail.com', '0123456789', 'Mathieu', 'Baudoin', '06 11 22 33 44', 'admin'),
+           ('jane.smith@email.com', 'pass456', 'Jane', 'Smith', '2222222222', 'admin'),
+           ('alice.jones@email.com', 'pass789', 'Alice', 'Jones', '3333333333', 'employee'),
+           ('bob.lee@email.com', 'pass000', 'Bob', 'Lee', '4444444444', 'employee'),
+           ('sarah.connor@email.com', 'pass111', 'Sarah', 'Connor', '5555555555', 'employee'),
+           ('tony.stark@email.com', 'ironman', 'Tony', 'Stark', '6666666666', 'employee'),
+           ('bruce.wayne@email.com', 'batman', 'Bruce', 'Wayne', '7777777777', 'user'),
+           ('clark.kent@email.com', 'superman', 'Clark', 'Kent', '8888888888', 'user'),
+           ('diana.prince@email.com', 'wonder', 'Diana', 'Prince', '9999999999', 'user'),
+           ('peter.parker@email.com', 'spiderman', 'Peter', 'Parker', '1010101010', 'user'),
+           ('user@email.com', 'userPass', 'User', 'User', '1212121212', 'user')
+    ;
 
 -- Commit if successful
 COMMIT;
+
+-- If something fails instead
+ROLLBACK;
