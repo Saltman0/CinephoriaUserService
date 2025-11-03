16cfff2e5381ca777d7f3befd44f23e1ed8511a7
Saltman0 <baudoin.mathieu@protonmail.com>
2025-11-04 00:52:45 +0100

    ✨ Reset password controller : resetPassword method added

--- src/controllers/password.controller.ts
+++ src/controllers/password.controller.ts
@@ -3,8 +3,7 @@ import crypto from "crypto";
 import * as userRepository from "../repository/user.repository";
 import * as resetPasswordRepository from "../repository/resetPassword.repository";
 
-
-export async function forgotPassword(req: Request, res: Response) {
+export async function forgotPassword(req: Request, res: Response): Promise<void> {
     try {
         const user = await userRepository.findUserByEmail(req.body.email);
 
@@ -14,11 +13,32 @@ export async function forgotPassword(req: Request, res: Response) {
 
             await resetPasswordRepository.insertResetPassword(token, expireTime, user.id);
 
-            const resetLink = `http://localhost:4200/reset-password?token=${token}`
+            const resetLink = `http://localhost:4200/reset-password?token=${token}`;
 
             res.status(200).json(resetLink);
         } else {
             res.status(404).json({error: `User not found`});
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+export async function resetPassword(req: Request, res: Response): Promise<void> {
+    try {
+        const resetPassword = await resetPasswordRepository.findResetPasswordByToken(req.body.token);
+
+        if (resetPassword !== null) {
+            if (Date.now() < resetPassword.expireTime.getTime()) {
+                await userRepository.updateUser(resetPassword.userId, null, req.body.password, null, null, null, null)
+                res.status(200).json({ message: 'Password successfully reset' });
+            } else {
+                res.status(400).json({ error: `Expired token` });
+            }
+        } else {
+            res.status(400).json({ error: `Invalid token` });
         }
     } catch (error) {
         if (error instanceof Error) {
