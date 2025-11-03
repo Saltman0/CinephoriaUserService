0cea1293fd769d5616e323f07caa0a1b8ee2c204
Saltman0 <baudoin.mathieu@protonmail.com>
2025-11-04 00:52:26 +0100

    ✨ Reset password repository : findResetPasswordByToken method added

--- src/repository/resetPassword.repository.ts
+++ src/repository/resetPassword.repository.ts
@@ -1,6 +1,24 @@
+import {eq} from "drizzle-orm/sql/expressions/conditions";
 import * as resetPasswordFactory from "../factory/resetPassword.factory";
 import {database} from "../config/database";
 import {resetPassword} from "../schema/reset-password";
+
+export async function findResetPasswordByToken(token: string) {
+    try {
+        const result = await database
+            .select()
+            .from(resetPassword)
+            .where(eq(resetPassword.token, token));
+
+        if (result.length === 0) {
+            return null;
+        }
+
+        return result[0];
+    } catch (error) {
+        throw error;
+    }
+}
 
 export async function insertResetPassword(token: string, expireTime: Date, userId: number) {
     const preparedInsertResetPassword = await database
